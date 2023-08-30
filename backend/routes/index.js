92ed864036d1eab0e2a9d578d101d536a3706ef5
mshuler <shulermj@gmail.com>
2023-08-29 18:16:18 -0700

    three

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,9 @@
 // backend/routes/index.js
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
 
 // router.get('/hello/world', function(req, res) {
 //   res.cookie('XSRF-TOKEN', req.csrfToken());
@@ -17,4 +20,9 @@ router.get("/api/csrf/restore", (req, res) => {
     });
   });
   // ...
+
+  router.post('/test', function(req, res) {
+    res.json({ requestBody: req.body });
+  });
+  
 module.exports = router;
